d388897806cdf7431b032838c7dfcdc52dacb0e6
atwani <ochukuta@gmail.com>
2023-06-06 09:45:01 +0300

    Add files via upload
    Added the initial files of the model

---
+++ api.py
@@ -0,0 +1,34 @@
+import sys
+import flask
+from flask import request
+import io 
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+#import tensorflow as tf
+import joblib
+import json
+import jsonify
+app = flask.Flask(__name__)
+model = joblib.load(r'C:/xampp/htdocs/moodle/mod/adaptlesson/Linear_model')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	features = flask.request.get_json(force=True)
+	data= pd.DataFrame([features])
+	data1 = data.to_numpy()
+	scaler=StandardScaler()
+	scaler_datax=scaler.fit_transform(data1)
+	prediction=model.predict(scaler_datax)
+	ts=np.array2string(prediction)
+	
+	return ts
+	#event=np.array(event)
+	#data={'Total1':[0],'File':[0],'File submissions':[0],'System':[0]}
+	
+
+
+if __name__=='__main__':
+	app.run(port= 3000, debug=True)
---
+++ python_file.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+import pickle
+
+pickled_model = pickle.load(open('model_linear.pkl', 'rb'))
+data = np.array([[sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5] ]])
+prediction=pickled_model.predict(data);
+print(prediction)
