69cfff1ae5e63795aa966c6d1185d64fae283a94
atwani <ochukuta@gmail.com>
2023-08-09 17:33:40 +0300

    Update python_file.py

--- python_file.py
+++ python_file.py
@@ -7,4 +7,4 @@ import pickle
 pickled_model = pickle.load(open('model_linear2.pkl', 'rb'))
 data = np.array([[sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5] ]])
 prediction=pickled_model.predict(data);
-print(prediction)
+#print(prediction)
