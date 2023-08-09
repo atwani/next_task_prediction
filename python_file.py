6bb72426581183be99767a42d54385f95c363128
atwani <ochukuta@gmail.com>
2023-08-09 16:50:22 +0300

    Update python_file.py

--- python_file.py
+++ python_file.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 import pickle
 
-pickled_model = pickle.load(open('model_linear.pkl', 'rb'))
+pickled_model = pickle.load(open('model_linear2.pkl', 'rb'))
 data = np.array([[sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5] ]])
 prediction=pickled_model.predict(data);
 print(prediction)
